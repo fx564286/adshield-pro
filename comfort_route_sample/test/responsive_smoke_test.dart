e92e416d24d7cae2a1d496ef3aef9a8d91f41a35
fx564286 <fx564286@gmail.com>
2026-08-16 14:57:38 +0900

    Update responsive smoke test for pleasant UI

--- comfort_route_sample/test/responsive_smoke_test.dart
+++ comfort_route_sample/test/responsive_smoke_test.dart
@@ -11,7 +11,7 @@ void main() {
   ];
 
   for (final size in sizes) {
-    testWidgets('renders without exceptions at ${size.width}x${size.height}',
+    testWidgets('renders pleasant UI without exceptions at ${size.width}x${size.height}',
         (tester) async {
       tester.view.devicePixelRatio = 1.0;
       tester.view.physicalSize = size;
@@ -21,8 +21,10 @@ void main() {
       await tester.pumpWidget(const ComfortRouteApp());
       await tester.pumpAndSettle();
 
+      expect(find.text('쾌적길'), findsOneWidget);
+      expect(find.text('덜 덥고, 덜 힘든 길'), findsOneWidget);
       expect(find.text('어디로 갈까요?'), findsOneWidget);
-      expect(find.text('지도'), findsOneWidget);
+      expect(find.text('길찾기'), findsOneWidget);
       expect(tester.takeException(), isNull);
     });
   }
